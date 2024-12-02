9f4ba63feaaa0e65e0a76df0c9e700900270961d
sunil3696 <sunilstark3696@gmail.com>
2024-12-01 20:05:30 -0500

    Initial Commit

---
+++ CivicCircleApp/CivicCircleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CivicCircleAppApp.swift
+//  CivicCircleApp
+//
+//  Created by Sunil Balami on 2024-12-01.
+//
+
+import SwiftUI
+
+@main
+struct CivicCircleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CivicCircleAppTests/CivicCircleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CivicCircleAppTests.swift
+//  CivicCircleAppTests
+//
+//  Created by Sunil Balami on 2024-12-01.
+//
+
+import Testing
+@testable import CivicCircleApp
+
+struct CivicCircleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
